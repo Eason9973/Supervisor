974467baa390c0156a2b2dece2861b7c0574b587
Eason9973 <eason9973@gmail.com>
2016-12-26 20:14:04 +0800

    Revert "Update: 新增远程重启功能，UI更友好。"
    This reverts commit 3e3a0b8c935f24d22c78da21c54604b4d8adcea8.

--- WindowsFormsApplication1/Properties/Resources.Designer.cs
+++ WindowsFormsApplication1/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace Supervisor.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap app {
-            get {
-                object obj = ResourceManager.GetObject("app", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
